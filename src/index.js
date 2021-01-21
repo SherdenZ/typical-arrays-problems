bdbc200ecfa5dd244f3a60bb91d7a0e54ddfb7e4
SHerdenZ <zuhio76@gmail.com>
2021-01-21 18:12:21 +0400

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = '') {
+    if (array == '') {
+      return 0;
+    };
+    let newArr = array;
+    let lowElem = newArr[0];
+   
+    for (i = 1; i < newArr.length; i++) {
+     if (lowElem > newArr[i]) {
+        lowElem = newArr[i];
+      }; 
+    };
+  
+    return lowElem;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = '') {
+  if (array == '') {
+    return 0;
+  };
+  let newArr = array;
+  let higElem = newArr[0];
+ 
+  for (i = 1; i < newArr.length; i++) {
+   if (higElem < newArr[i]) {
+    higElem = newArr[i];
+    }; 
+  };
+
+  return higElem;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = '') {
+  if (array == '') {
+    return 0;
+  };
+  let newArr = array;
+  let sumOfArr = (sum, currentValue) => sum + currentValue;
+  let average = (newArr.reduce(sumOfArr))/newArr.length;
+  return average;
 }
